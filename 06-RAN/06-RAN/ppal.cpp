7076a4b70f533cd8995aba2fc003a45063aa81d7
German <german.rocha18@gmail.com>
2016-04-19 03:30:33 +0000

    The code is finished

--- 06-RAN/06-RAN/ppal.cpp
+++ 06-RAN/06-RAN/ppal.cpp
@@ -6,14 +6,16 @@
 
 #include <iostream>
 #include <stdio.h>
-#include <conio.h>
 #include <time.h>
 
 using namespace std;
 
+string checkString(string word);
+
 int main()
 {
 	int a = 0, b = 0, pet_flor = 0, guess = 0;
+	string repetir = "si";
 	srand(time(NULL));
 	int dado[5];
 	
@@ -31,21 +33,50 @@ int main()
 	cout<<"\nAdivine el truco del juego, si adivina obtiene $500.00 pesos.";
 	cout<<"\n\nEstos son los resultados de 6 dados lanzados:\n\n";
 
-	for(int i = 0; i < 5; i++)
+	while(repetir == "si")
 	{
-		cout<<"Dado "<<i + 1<<"\t";
+		for(int i = 0; i < 5; i++)
+		{
+			cout<<"Dado "<<i + 1<<"\t";
+		}
+		
+		cout<<"Petalos de una flor\n";
+		
+		for(int i = 0; i < 5; i++)
+		{
+			cout<<"  "<<dado[i]<<" \t";
+		}
+		cout<<"\nCuantos petalos crees que hayan? ";
+		cin>>guess;
+		
+		if(guess == pet_flor)
+		{
+			cout<<"Correcto! Ya estas listo para adivinar el truco del juego o quieres una iteracion mas? (si/no) ";
+			cin>>repetir;
+			
+			repetir = checkString(repetir);
+		}
+		else
+		{
+		 	cout<<"Lo siento, los petalos son: "<<pet_flor<<" \nQuieres una iteracion mas? (si/no) ";	
+		 	cin>>repetir;
+		 	
+		}
+		
 	}
 	
-	cout<<"Petalos de una flor\n";
-	
-	for(int i = 0; i < 5; i++)
-	{
-		cout<<"  "<<dado[i]<<" \t";
-	}
-	cout<<"\nCuantos petalos crees que hayan? ";
-	cin<<guess;
-	cout<<"\nPresiona ENTER";
-	getch();
+	cout<<"Escribe el truco del juego";
 
 	return 0;
+}
+
+string checkString(string word)
+{
+	while(word == "Si" || word == "SI" || word == "sI")
+	{
+		cout<<"Opcion invalida! Debe ingresar \'si\' en minusculas\n";
+		cin>>word;
+		
+	}
+	return word;
 }
