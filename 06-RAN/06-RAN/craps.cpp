1c42902f605f5fc92ddea2d0b06a4b74a559e704
German <german.rocha18@gmail.com>
2016-04-23 21:08:10 +0000

    This is a huge change to craps.cpp

--- 06-RAN/06-RAN/craps.cpp
+++ 06-RAN/06-RAN/craps.cpp
@@ -1,65 +1,138 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <stdio.h>
 #include <math.h>
 #include <time.h>
-#include <cconio>
+#include <conio.h>
+#include <string>
 
 using namespace std;
 
-void displayBote(float bote_usuario, float bote_casa, float bote_total);
+int getRandomNumber();
+bool checkContinue(string word);
+void displayBote(float bote_usuario, float bote_total);
 
 int main()
 {
-	int m1, m2, objetivo = 0, tiro = 0;
-	float bote_total = 0, bote_casa = 10, bote_usuario = 0;
-	bool ganar = false;
-
-	while (ganar == false)
-	{
-		tiro += 1;
-		srand(time(NULL));
+	int dado1, dado2, suma = 0, objetivo = 0, tiro = 1;
+	float bote_total = 10, bote_usuario = 5;
+	string respuesta = "";
 	
-		m1 = 1 + rand()%6;
-		m2 = 1 + rand()%6;
-		objetivo = m1 + m2;
+	cout<<"\t\tJUGEMOS CRAPS\n";
+	
+	while (true)
+	{
+		system("cls");
 
-		cout<<"Jugemos Craps\n\n";
-		cout<<"Para comenzar debes apostar 5 pesos\n";
+		cout<<"\t\tJUGEMOS CRAPS\n";
 		
+		bote_total += bote_usuario;
 
+		displayBote(bote_usuario, bote_total);
 
-		bote_usuario = 5;
-		bote_total = bote_usuario + bote_casa;
-
-		displayBote(bote_usuario, bote_casa, bote_total);
-
+		cout<<"\nPara comenzar presione ENTER\n";
+		getch();
+		
+		dado1 = getRandomNumber();
+		dado2 = getRandomNumber();
+		
+		suma = dado1 + dado2;
+		
+		system("cls");
 		cout<<"\nEl resultado de sus dados es:\n";
-		cout<<"Dado 1: "<<m1<<"\nDado 2: "<<m2<<"\n\n";
+		
+		cout<<"Dado 1: "<<dado1<<"\nDado 2: "<<dado2<<"\n\n";
 
-		if((objetivo == 7) || (objetivo == 11) && (tiro == 1))
+		if((suma == 7) || (suma == 11))
 		{
-			ganar = true;
-			cout<<"Ganaste!\nPresiona ENTER para continuar";
+			bote_usuario = (bote_total / 2);
+			
+			cout<<"Ganaste!\nBote propio: "<<bote_usuario<<" Pesos\nDesea continuar apostando? (si/no)\n";
+			cin>>respuesta;
+			
+			if(checkContinue(respuesta))
+			{
+				system("cls");
+			}
+			
+			else if (checkContinue(respuesta) == false)
+			{
+				break;
+			}
+
+		}
+		else if((suma == 2) || (suma == 3) || (suma == 12))
+		{
+			cout<<"Perdiste!\nDesea continuar apostando? (si/no)\n";
+			cin>>respuesta;
+			
+			if(checkContinue(respuesta))
+			{
+				cout<<"Presiona ENTER para continuar\n";
+			}
+			
+			else if (checkContinue(respuesta) == false)
+			{
+				break;
+			}
 			
 			getch();
-
-        	bote_total = bote_usuario + bote_casa;
-			bote_usuario = (bote_total / 2);
-
-			displayBote(bote_usuario, bote_casa, bote_total);
 		}
-		else if((objetivo == 2) || (objetivo == 3) || (objetivo == 12) && (tiro == 1))
+		else
 		{
-			ganar  = false;
+			objetivo = suma;
+			tiro += 1;
+		
+			break;
 		}
-
-		getch();
 				
 	}
+	
+	if(tiro > 1)
+	{
+		cout<<"Su objetivo es "<< objetivo;
+		getch();
+				
+		while(suma != objetivo)
+		{
+			
+		}	
+		
+	}
 
+	cout<<"\nGracias por jugar!\n";
+	
 	return 0;
 }
 
-void displayBote(float bote_usuario, float bote_casa, float bote_total)
+void displayBote(float bote_usuario, float bote_total)
 {
-	cout<<"\nBote total: "<<bote_total<<" pesos\tBote propio: "<<bote_usuario<<" pesos\tBote casa: "<<bote_casa<<" pesos\n";
+	cout<<"\nBote total: "<<bote_total<<" pesos\tBote propio: "<<bote_usuario<<" pesos\n";
+}
+
+bool checkContinue(string word)
+{
+	bool accepted;
+	
+	if(word == "si" || word == "Si" || word == "sI" || word == "SI")
+	{
+		accepted = true;
+	}
+	else
+	{
+		accepted = false;
+	}
+	
+	return accepted;
+}
+
+int getRandomNumber()
+{
+	int number;
+	srand(time(NULL));
+		
+	number = 1 + rand()%6;
+	
+	return number;	
 }
