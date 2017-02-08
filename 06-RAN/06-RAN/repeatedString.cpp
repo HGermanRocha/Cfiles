3eb3016ff8709b393222847c91be1daeed2cfab7
German <german.rocha18@gmail.com>
2017-02-08 18:24:09 +0000

    New File

--- 06-RAN/06-RAN/repeatedString.cpp
+++ 06-RAN/06-RAN/repeatedString.cpp
@@ -23,28 +23,63 @@
 
 using namespace std;
 
-
 int main(){
-    int counter = 0, x = 0, i = 0;
+    int i = 0, a = 0, j = 0;
+    long int div, counter = 0,x = 0;
     string s;
     cin >> s;
-    long n;
+    long int n;
     cin >> n;
     
-    while(x < n)
+    if(n < 20)
     {
-        while(i < s.length() && x < n)
+        while(x < n)
+        {
+            while(i < s.length() && x < n)
+            {
+                if(s[i] == 'a')
+                {
+                    counter++;   
+                }
+                i++;
+                x++;    
+            }
+            i = 0;
+        }
+    }
+    else
+    {
+    	div = n/s.length();
+        for(int i = 0; i < s.length(); i++)
         {
             if(s[i] == 'a')
-            {
-                counter++;   
-            }
-            i++;
-            x++;    
+                a++;
         }
-        i = 0;
+        //cout<<"a: "<<a<<"\t\tdiv:"<<div<<"\t\tn: "<<n<<"\t\tlen"<<s.length()<<endl;
+        counter = a*div; 
+    	//cout<<"Counter: "<<counter<<endl;
+		if(n%s.length() != 0)    
+		{
+            //cout<<"test";
+            
+	        x = div * s.length();
+            //cout<<"x"<<x<<endl;
+	        while(x < n)
+	        {
+	            while(j < s.length() && x < n)
+	            {
+	                if(s[j] == 'a')
+	                {
+	                    counter++;   
+	                }
+	                j++;
+	                x++;    
+	            }
+	            j = 0;
+	        }
+	    }
+        
     }
-    
     cout<<counter;
     
     return 0;
