2a40a5157f2f3c9a76621e4a1283095f7bc6e40e
German <german.rocha18@gmail.com>
2017-02-13 21:42:07 +0000

    Details

--- 06-RAN/06-RAN/HackerRank/MiniMax.cpp
+++ 06-RAN/06-RAN/HackerRank/MiniMax.cpp
@@ -21,7 +21,7 @@ int main()
     {
         for(int i = 0; i < 4; i++)
         {
-            if(array[i] < array[i + 1])
+            if(array[i] <= array[i + 1])
             {   
                 counter++;
             }
