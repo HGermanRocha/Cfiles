a9e3ddcd4a8229f9a6e45dc8214c916b448fe56d
German <german.rocha18@gmail.com>
2017-02-27 21:51:36 +0000

    Convirtiendo a posfija

--- 06-RAN/06-RAN/Pila.cpp
+++ 06-RAN/06-RAN/Pila.cpp
@@ -13,7 +13,6 @@ class Pila
 		int pos;
 		char arreglo[MAX] = {0};
 	public:
-		
 		void crearPila()
 		{
 			pos = 0;
@@ -57,13 +56,33 @@ class Pila
 		{
 			return arreglo[pos-1];
 		}
-		bool balanceada()
+
+		void imprimir()
+		{
+			for(int i = pos-1; i >= 0; i--)
+			{
+				cout<<arreglo[i]<<endl;
+			}
+		}
+};
+
+class Expresion
+{
+	private:
+		string expresion;
+		
+	public:
+		Expresion(string expresion="{3*[(9-7)+(8*3)]}")
+		{
+			this->expresion = expresion;
+		}
+		bool estaBalanceada()
 		{
 			int aberturas = 0, cerraduras = 0;
 			int aber1 = 0, cer1 = 0;
 			char type;
 			
-			string expresion = "{3*[(9-7)+(8*3)]}";
+			//string expresion = "{3*[(9-7)+(8*3)]}";
 			
 			for(int i = 0; i < expresion.length(); i++)
 			{
@@ -120,34 +139,42 @@ class Pila
 			
 			return aberturas==cerraduras && aber1==cer1;
 		}
-
-		void imprimir()
+		
+		bool esMayor(char operador1, char operador2)
 		{
-			for(int i = pos-1; i >= 0; i--)
-			{
-				cout<<arreglo[i]<<endl;
-			}
+			
 		}
 };
 
 int main()
 {
-	Pila p;	
+	string infija = "4*5/(4+6)";
+	Expresion expresion(infija);	
+	string expresionPosfija=infija;
+	int counter = 0;
 	
-	p.imprimir();
-	
-	if(p.balanceada())
+	if(expresion.estaBalanceada())
 	{
 		cout<<"La expresion esta balanceada!";
+		cout<<endl;
+		for(int i = 0; i < infija.length(); i++)
+		{
+			if((int)infija[i] >= 48 && (int)infija[i]<=57)
+			{
+				expresionPosfija[counter] = infija[i];
+				counter++;
+			}
+		}
+		
 	}
 	else
 	{
 		cout<<"No esta balanceada";
 	}
-	
+	//cout<<expresionPosfija;
 	cout<<endl;
 	
-	system("pause");
+	//system("pause");
 	
 	return 0;
 }
