91bcca753eb017c263e2500cb75ecdddf7a64fc2
German <german.rocha18@gmail.com>
2017-03-09 00:12:25 +0000

    NEw file

---
+++ 06-RAN/06-RAN/Cola.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef int TIPO_DE_DATO;
+
+const int MAXTAMQ = 5;
+
+class TDA_Cola 
+{
+	private:
+		int frente;
+		int final;
+		int numElementos;
+		TIPO_DE_DATO Arreglo[MAXTAMQ];	
+	public:
+	    int siguiente(int parametro)
+	    {
+	        return (parametro + 1) % MAXTAMQ;
+	    }
+	    void crearCola()
+	    {
+	        frente = 0;
+	        final = MAXTAMQ - 1;
+	        numElementos = 0;
+	    }
+	    bool estaVacia()
+	    {
+	        return frente==siguiente(final);
+	    }
+	    bool estaLlena()
+	    {
+	        return frente == siguitente(siguiente(final));
+	    }
+	    bool insertar(TIPO_DE_DATO x)
+	    {
+	        if(estaLlena())
+	        {
+	            return false;
+	        }
+	        
+	        final = siguiente(final);
+	        Arreglo[final] = x;
+	        numElementos++;
+	        return true;
+	    }
+	    bool eliminar()
+	    {
+	        if(estaVacia())
+	        {
+	            return false;
+	        }
+	        
+	        frente = siguiente(frente);
+	        numElementos--;
+	        return true;
+	    }
+	    TIPO_DE_DATO getNumElementos()
+	    {
+	        return numElementos;
+	    }
+};
+
+int main()
+{
+	
+	
+	return 0;
+}
