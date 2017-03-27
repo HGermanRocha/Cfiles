34deaf6d2858e679ec95c778125b325b065ad7a1
German <german.rocha18@gmail.com>
2017-03-27 23:13:50 +0000

    Almost done

--- 06-RAN/06-RAN/Lista.cpp
+++ 06-RAN/06-RAN/Lista.cpp
@@ -54,15 +54,32 @@ class TDA_Lista_Simple
         }
         void insertarCabezaLista(Dato entrada)
         {
+            Nodo* nuevo;
             
+            nuevo = new Nodo(entrada);
+            
+            nuevo->ponerEnlace(primero);
+            
+            primero = nuevo;
         }
         void insertarUltimo(Dato entrada)
         {
             
+            Nodo* ultimo = this->ultimo();  //->this->ultimo() llama a otra funcion dentro de la clase.
+            ultimo->ponerEnlace(new Nodo(entrada));
         }
         Nodo* ultimo()
         {
+            Nodo* p;
             
+            p = primero;
+            
+            while(p->enlaceNodo() != NULL)
+            {
+                p = p->enlaceNodo();
+            }
+            
+            return p;
         }
         void insertarLista(Nodo* anterior, Dato entrada)
         {
@@ -82,17 +99,42 @@ class TDA_Lista_Simple
         }
         void mostrar()
         {
+            Nodo* actual = primero;
+            
+            while(actual != NULL)
+            {
+                cout<<actual->datoNodo()<<", ";
+                
+                actual = actual->enlaceNodo();
+            }
             
         }
 };
 
 int main()
 {
+    Dato valor;
     TDA_Lista_Simple l;
     
-    Nodo* primero = new Nodo(19);
+    cout<<"Inicializar lista con el valor: ";
+    cin>>valor;
+    
+    Nodo* primero = new Nodo(valor);
     
     l.crearLista(primero);
+    
+    for(int i = 0; i < 3; i++)
+    {
+        cout<<endl<<"Valor: ";
+        cin>>valor;
+        
+        l.insertarCabezaLista(valor);
+        
+    }
+    
+    l.insertarUltimo(29);
+    
+    l.mostrar();
     
     return 0;
 }
